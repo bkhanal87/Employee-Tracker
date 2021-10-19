14f536980969f7b31f47d748a2c5422bda0a8d5c
Basu Khanal <87610840+bkhanal87@users.noreply.github.com>
2021-10-19 13:49:01 -0400

    added codes for start function

--- db/server.js
+++ db/server.js
@@ -7,23 +7,88 @@ const inquirer = require("inquirer");
 // Import and require mysql2
 const mysql = require("mysql2");
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+// Import and require console.table package
+
+const cTable = require("console.table");
+// const Connection = require("mysql2/typings/mysql/lib/Connection");
+
+// const PORT = process.env.PORT || 3001;
+// const app = express();
 
 // Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection(
   {
-    host: '127.0.0.1',
-    port: 33070,
+    host: 'localhost',
+    port: 3306,
     // MySQL username,
     user: 'root',
     // TODO: Add MySQL password here
-    password: 'secret',
-    database: 'movies_db'
+    password: '@9803656593Bk',
+    database: 'employeeTracker'
   },
-  console.log(`Connected to the movies_db database.`)
+  console.log(`Connected to the employeeTracker database.`)
 );
+
+// Functionalities here
+
+function start(){
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "start",
+                message: "What would you like to do?",
+                choices: ["View", "Add", "Update", "Quit"]
+            }
+        ]).then (function(res){
+            switch(res.start){
+                case "View":
+                    view();
+                    break;
+                case "Add":
+                    add();
+                    break;
+                case "Update":
+                    updateEmployee();
+                    break;
+                case "Quit":
+                    console.log("--------------------------");
+                    console.log("All done");
+                    console.log("--------------------------");
+                    break;
+                default:
+                    console.log("default");
+            }
+        });
+}
+
+function view(){
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "view",
+                message: "Select one to view:",
+                choices: ["All employees", "By department", "By role"]
+            }
+        ]).then(function(res){
+            switch(res.view){
+                case "All employees":
+                    viewAllEmployees();
+                    break;
+                case "By department":
+                    viewByDepartment();
+                    break;
+                case "By role"
+                    viewByRole();
+                default:
+                    console.log("default");
+            }
+
+        })
+}
+
