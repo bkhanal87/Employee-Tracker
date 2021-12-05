537dd3548d7c287cfb1db479cafac262031081b4
Basu Khanal <87610840+bkhanal87@users.noreply.github.com>
2021-12-05 11:41:18 -0500

    saving latest changses

---
+++ db/schema.sql
@@ -0,0 +1,37 @@
+-- creating a database employee_tracker --
+
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+-- creating three tables: department, role, and employee --
+
+-- department table --
+CREATE TABLE department(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) UNIQUE NOT NULL
+);
+
+-- role table -- 
+
+CREATE TABLE role(
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  PRIMARY KEY (id)
+);
+
+-- employee table --
+CREATE TABLE employee(
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id VARCHAR(30),
+  manager_id INT,
+  PRIMARY KEY (id)
+  );
+
+
+
